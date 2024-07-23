78d7df50d1ccc85e640e4ed106aec2ffcf2d0d51
pombschool <101295533+pombschool@users.noreply.github.com>
2024-07-23 16:10:17 -0400

    Fixed a mistake in Project 4

--- Tester.java
+++ Tester.java
@@ -34,7 +34,7 @@ public class Tester
     }
 
     public static void ArithmeticAssignment() {
-        System.out.println("PROJECT 3:");
+        System.out.println("PROJECT 4:");
         System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 = " + (79 + 3 * (4 + 82 - 68) - 7 + 19));
         System.out.println("(179 + 21 + 10) / 7 + 181 = " + ((179 + 21 + 10) / 7 + 181));
         System.out.println("10389 * 56 * 11 + 2246 = " + (10389 * 56 * 11 + 2246));
